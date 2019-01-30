785ac274beee6e8252fcae61166c90c60e810f66
icantnotisdie <47150604+icantnotisdie@users.noreply.github.com>
2019-01-30 15:37:23 +0800

    上传文件
    第一次使用github上传文件

---
+++ ConstantUtils.java
@@ -0,0 +1,21 @@
+package com.ljl.my.book.commons.constant;
+
+public class ConstantUtils {
+
+    public static final String SESSION_USER = "tbUser";
+    // 管理员的名称
+    public static final String SESSION_TBADMINISTRATOR = "tbAdministrator";
+    // 书籍的名称
+    public static final String SESSION_NEWBOOKS = "tbbook";
+    // 注册的提示信息
+    public static final String SESSION_REGISTERMASSAGE = "registerMassage";
+    // 登录的提示信息
+    public static final String SESSION_LOGINMASSAGE = "loginMessage";
+    // 个人书籍的集合
+    public static final String SESSION_TBBOOKLIST = "tbBookList";
+    // 所有书籍的集合
+    public static final String SESSION_MORETBBOOKLIST = "moreTbBookList";
+    // 所有书籍类别的集合
+    public static final String SESSION_MORETBCATEGORIES = "moreTbCategories";
+
+}
